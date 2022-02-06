d31bf066fd4ebed51447b6d7bf47b3011bd6acdf
kkenitsa <kristina.bits97@gmail.coml>
2022-02-06 21:03:00 +0200

    first commit

---
+++ cypress/pages/pageLogin.js
@@ -0,0 +1,38 @@
+class LoginForm {
+    nameForm() {
+        return cy.get('#userName')
+    }
+
+    emailForm() {
+        return cy.get('#userEmail')
+    }
+
+    currentAddressForm() {
+        return cy.get('#currentAddress-wrapper  #currentAddress')
+    }
+
+    permanentAddressForm() {
+        return cy.get('#permanentAddress-wrapper  #permanentAddress')
+    }
+
+    nameResult() {
+        return cy.get('#name')
+    }
+
+    emailResult() {
+        return cy.get('#email')
+    }
+
+    currentAddressResult() {
+        return cy.get('#output  #currentAddress')
+    }
+
+    permanentAddressResult() {
+        return cy.get('#output  #permanentAddress')
+    }
+
+    buttonSubmit() {
+        return cy.get('#submit')
+    }
+}
+export default LoginForm;

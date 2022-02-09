1b24cfbcc7f945a0552018e159b60e895a85dd7d
kkenitsa <kristina.bits97@gmail.com>
2022-02-09 21:48:51 +0200

    master clear

--- cypress/pages/testData.js
+++
@@ -1,8 +0,0 @@
-const faker = require('faker');
-export const USER_DATA = {
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  currentAddress: faker.address.streetAddress(),
-  permanentAddress: faker.address.streetAddress(),
-};
-export const URL = 'https://demoqa.com/text-box';

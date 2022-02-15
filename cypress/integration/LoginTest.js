3fa85cb2b7e82985481a04ecb9efe19dccf89d8f
kkenitsa <kristina.bits97@gmail.com>
2022-02-15 15:46:40 +0200

    code refactoring

--- cypress/integration/LoginTest.js
+++ cypress/integration/LoginTest.js
@@ -1,5 +1,5 @@
 import LoginForm from '../pages/pageLogin.js';
-import {USER_DATA, TEXT_BOX_URL} from '../pages/testData';
+import {USER_DATA, TEXT_BOX_URL, OTHER_USER_DATA} from '../pages/testData';
 const loginForm = new LoginForm;
 
 describe("Log in", () => {
@@ -10,12 +10,12 @@ describe("Log in", () => {
     it("Verify that entering data will submit", () => {
         loginForm.nameForm().type(USER_DATA.name).should("be.visible");
         loginForm.emailForm().type(USER_DATA.email).should("be.visible");
-        loginForm.currentAddressForm().type(USER_DATA.currentAddress).should("be.visible");
-        loginForm.permanentAddressForm().type(USER_DATA.permanentAddress).should("be.visible");
+        loginForm.currentAddressForm().type(OTHER_USER_DATA.currentAddress).should("be.visible");
+        loginForm.permanentAddressForm().type(OTHER_USER_DATA.permanentAddress).should("be.visible");
         loginForm.buttonSubmit().should("be.visible").click();
         loginForm.nameResult().contains(USER_DATA.name);
         loginForm.emailResult().contains(USER_DATA.email);
-        loginForm.currentAddressResult().contains(USER_DATA.currentAddress);
-        loginForm.permanentAddressResult().contains(USER_DATA.permanentAddress);
+        loginForm.currentAddressResult().contains(OTHER_USER_DATA.currentAddress);
+        loginForm.permanentAddressResult().contains(OTHER_USER_DATA.permanentAddress);
     });
 })
--- cypress/pages/testData.js
+++ cypress/pages/testData.js
@@ -2,7 +2,11 @@ const faker = require('faker');
 export const USER_DATA = {
     name: faker.name.findName(),
     email: faker.internet.email(),
+};
+
+export const OTHER_USER_DATA = {
     currentAddress: faker.address.streetAddress(),
     permanentAddress: faker.address.streetAddress(),
 };
+
 export const TEXT_BOX_URL = 'text-box';
